61c01250778be0f486f40f6cd9d078a0e48d629e
Imrich Toth <imrich.toth2@mail.dcu.ie>
2021-01-10 15:29:46 +0000

    Added selection_sort.c

---
+++ src/selection_sort.c
@@ -0,0 +1,15 @@
+void SelectionSt(int array[], int n)
+{
+   int i, j, min;
+   for (i = 0; i < n - 1; i++)
+   {
+      min = i;
+      for (j = i + 1; j < n; j++)
+         if (array[j] < array[min])
+            min = j;
+
+      int temp = array[min];
+      array[min] = array[i];
+      array[i] = temp;
+   }
+}
