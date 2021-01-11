15a9679ef1d3d43da8c4bfd76c84ffabee03aaa1
Imrich Toth <imrich.toth2@mail.dcu.ie>
2021-01-11 03:10:05 +0000

    Updated makefile

--- src/makefile.mk
+++ src/makefile.mk
@@ -2,6 +2,7 @@ prog: num_generator.c selection_sort.c insertion_sort.c main.c radix_sort.c shel
     gcc num_generator.c -o num_generator;
     gcc selection_sort.c -o selection_sort;
     gcc insertion_sort.c -o insertion_sort;
-    gcc radix_sort.c -o radix_sort;
     gcc main.c -o main;
+    gcc radix_sort.c -o radix_sort;
     gcc shell_sort.c -o shell_sort;
+
