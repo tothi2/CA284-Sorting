6a0af2b910ba501424929104c177f6c7fae67063
Imrich Toth <imrich.toth2@mail.dcu.ie>
2021-01-11 11:57:35 +0000

    Update num_generator.c

--- src/num_generator.c
+++ src/num_generator.c
@@ -11,7 +11,7 @@ int main(int argu, char *argv[])
 	arg = atoi(argv[1]);
 	srand(time(NULL));
 	int array = (int*)malloc(arg * sizeof(arg));
-	for(i = 0; i < arg; i++)
+	for(i = 0; i < arg; i++) //This is where I edited the code to half sort the elements, I replacaed i < arg with i < arg / 2
 	{
 		array = rand();
 		printf("%d\n", array);
